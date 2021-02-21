6284fe1655efccfa63d92ecf84c041cc9c2e048a
Your Name <tomek.groos@gmail.com>
2021-02-21 23:43:22 +0100

    validation mechanism works

--- main.js
+++ main.js
@@ -1,42 +1,4 @@
-// valid credit card numbers
-const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
-const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
-const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
-const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
-const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
 
-// invalid credit card numbers
-const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
-const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
-const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
-const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
-const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
-
-// Can be either valid or invalid
-const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
-const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
-const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
-const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
-const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
-
-// Array of The Arrays
-const batch = [
-  valid1,
-  valid2,
-  valid3,
-  valid4,
-  valid5,
-  invalid1,
-  invalid2,
-  invalid3,
-  invalid4,
-  invalid5,
-  mystery1,
-  mystery2,
-  mystery3,
-  mystery4,
-  mystery5,
-];
 
 const validateCard = (arr) => {
   let arrCopy = arr.slice();
@@ -67,18 +29,19 @@ const validateCard = (arr) => {
   const isValid = checkSum % 10 == 0 ? "card is valid" : "card is not valid";
 
   return isValid;
+  
 };
 
 /* validateCard(valid1);
-validateCard(invalid1); */
+validateCard(invalid1); 
 
-/* let cardCounter = 0;
+let cardCounter = 0; */
 
-batch.forEach((card) => {
-  cardCounter++; */
+/* batch.forEach((card) => {
+  cardCounter++; 
 
-/*   console.log(`Validation for the ${cardCounter} card: ${validateCard(card)}`)
-}); */
+ console.log(`Validation for the ${cardCounter} card: ${validateCard(card)}`)
+});  */
 
 // jump to the next number
 
@@ -106,28 +69,48 @@ const backSpace = (field, prevField) => {
 
 // move numbers into array
 
+let newArray = [];
+
+// validations
+
+
+
 const getValue = (event) => {
   const allNumbers = document.querySelectorAll("input");
+
+  
 
   const allNumbersArray = Array.from(allNumbers);
   event.preventDefault();
 
-  const newArray = allNumbersArray.map((el) => {
+  allNumbersArray
+    .map((el) => {
+      if(el.value.length < 1){
+        console.log("field cannot be empty!");
+        return;
+      }else{
+        return el.value;
+      }
+     
+    })
+    .map((el) => Number(el))
+    .map((el) => newArray.push(el));
 
-    return el.value
-   
-  }).map(el => Number(el))
-
-console.log(newArray);
-
-/* const stringToNumber = newArray.map((el) => Number(el))
-
-console.log(stringToNumber);
- */
-
+    console.log(newArray);
+    console.log(validateCard(newArray));
+    
+    // clear fields
+    allNumbers.forEach(el =>{
+      el.value = "";
+    })
 
 };
 
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById("getNumbers").addEventListener("click", getValue);
+  
 });
+
+
+
+
